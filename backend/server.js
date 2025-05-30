8031583a2b3ea17fff15878155043ce461b19890
paulo sergio <psergio001192@gmail.com>
2025-05-30 20:40:30 -0300

    2 config do server

--- backend/server.js
+++ backend/server.js
@@ -31,6 +31,6 @@ app.use("/api/v1/dashboard", dashboardRoutes);
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+//app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 module.exports = app; // Adicione esta linha para exportar o app
